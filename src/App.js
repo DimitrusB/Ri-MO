89c439fe7dbd437ee552a4a1b73c4ee602494034
DimitrusB <dimitrussboss@gmail.com>
2024-11-15 15:30:28 +0300

    get

--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { fetchCharacters } from "./api";
+import Dring from "./img/8.gif";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const loadCharacters = async () => {
+      try {
+        const data = await fetchCharacters();
+        if (data && data.results) {
+          setCharacters(data.results);
+          console.log(data.results); // Проверяем, что мы устанавливаем в состояние
+        } else {
+          throw new Error("Данные не содержат свойства results");
+        }
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadCharacters(); // Вызов функции для загрузки персонажей
+  }, []); // Пустой массив зависимостей, чтобы вызвать эффект только при монтировании
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {loading ? (
+          <div>
+            <img
+              src={Dring}
+              alt="Загрузка..."
+              style={{ width: "200px", height: "200px" }} // Убедитесь, что размеры изображения заданы
+            />
+          </div>
+        ) : (
+          <>
+            <h1>Персонажи Рика и Морти</h1>
+            <ul>
+              {characters.length > 0 ? (
+                characters.map((character) => (
+                  <li key={character.id}>
+                    <a
+                      href={character.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {character.name}
+                    </a>
+                  </li>
+                ))
+              ) : (
+                <li>Нет персонажей для отображения</li>
+              )}
+            </ul>
+          </>
+        )}
       </header>
     </div>
   );
---
+++ src/api.jsx
@@ -0,0 +1,12 @@
+// URL API
+const url = 'https://rickandmortyapi.com/api';
+
+export const fetchCharacters = async () => {
+    const response = await fetch(url + "/character");
+    if (!response.ok) {
+      throw new Error("Ошибка сети");
+    }
+    const data = await response.json();
+    return data; // Убедитесь, что возвращается объект с `results`
+  };
+  
