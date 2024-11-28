609185c74158a2d27f777e005c3dbcff9b7ee46d
DimitrusB <dimitrussboss@gmail.com>
2024-11-28 11:19:54 +0300

    appfix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React from "react";
 import "./App.css";
-
-import { BrowserRouter, BrowserRouter as Router } from "react-router-dom";
+import home from "./img/home.svg";
+import { BrowserRouter as Router, Link, useLocation } from "react-router-dom";
 import RoutesPages from "./components/navigate";
 
 function App() {
+  const location = useLocation(); 
+
+  const getTitle = () => {
+    if (location.pathname === "/") {
+      return "Персонажи Рика и Морти";
+    } else {
+      return "Описание";
+    }
+  };
+
   return (
-    <BrowserRouter
-      future={{
-        v7_startTransition: true,
-      }}
-    >
-      <div className="App">
-        <header className="App-header">
-          <RoutesPages />
-        </header>
-      </div>
-    </BrowserRouter>
+    <div style={{ width: "100%", height: "376px", backgroundColor: "white" }}>
+      <Link to="/">
+        <img src={home} style={{ margin: "50px" }} alt="Home" />
+      </Link>
+      <h1 style={{ textAlign: "center", color: "black" }}>{getTitle()}</h1>
+    </div>
   );
 }
 
-export default App;
+const AppWrapper = () => (
+  <Router>
+    <App />
+    <div className="App">
+      <header className="App-header">
+        <RoutesPages />
+      </header>
+    </div>
+  </Router>
+);
+
+export default AppWrapper;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,12 +5,6 @@ import home from "../../img/home.svg";
 export const Home = () => {
   return (
     <div>
-      <div  style={{width:"100%", height:"376px", backgroundColor:"white"}}>
-        <Link to="/">
-          <img src={home} style={{ margin:"50px" }} alt="Home" />
-        </Link>
-        <h1 style={{ textAlign:"center", color:"black"}}>Персонажи Рика и Морти</h1>
-      </div>
       <div>
       <ListHeroes />
       </div>
